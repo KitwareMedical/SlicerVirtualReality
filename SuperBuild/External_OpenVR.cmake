92eb1b5d458cb177f27c4ce4ab9970d63f83cada
Csaba Pinter <csaba.pinter@queensu.ca>
2019-10-14 10:04:00 -0400

    ENH: Update OpenVR to 1.7.15
    Re https://github.com/KitwareMedical/SlicerVirtualReality/issues/77

--- SuperBuild/External_OpenVR.cmake
+++ SuperBuild/External_OpenVR.cmake
@@ -23,12 +23,12 @@ ExternalProject_Include_Dependencies(${proj} PROJECT_VAR proj DEPENDS_VAR ${proj
 if((NOT OPENVR_INCLUDE_DIR OR NOT OPENVR_LIBRARY)
    AND NOT ${CMAKE_PROJECT_NAME}_USE_SYSTEM_${proj})
 
-  set(_version "1.6.10")
+  set(_version "1.7.15")
 
   ExternalProject_Add(${proj}
     ${${proj}_EP_ARGS}
     URL "https://github.com/ValveSoftware/openvr/archive/v${_version}.tar.gz"
-    URL_MD5 "9fd0502585c7d63811cf915a4f8b7cc0"
+    URL_MD5 "88f30043996b4735baf59b020d13e25e"
     DOWNLOAD_DIR ${CMAKE_BINARY_DIR}
     SOURCE_DIR ${CMAKE_BINARY_DIR}/${proj}
     BUILD_IN_SOURCE 1
