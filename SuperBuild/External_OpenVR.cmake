0bf1969f66714baa1eca860e4370ca224febe748
Csaba Pinter <pinter.csaba@gmail.com>
2020-08-19 14:19:07 +0100

    ENH: Update OpenVR to 1.12.5

--- SuperBuild/External_OpenVR.cmake
+++ SuperBuild/External_OpenVR.cmake
@@ -23,12 +23,12 @@ ExternalProject_Include_Dependencies(${proj} PROJECT_VAR proj DEPENDS_VAR ${proj
 if((NOT OPENVR_INCLUDE_DIR OR NOT OPENVR_LIBRARY)
    AND NOT ${CMAKE_PROJECT_NAME}_USE_SYSTEM_${proj})
 
-  set(_version "1.7.15")
+  set(_version "1.12.5")
 
   ExternalProject_Add(${proj}
     ${${proj}_EP_ARGS}
     URL "https://github.com/ValveSoftware/openvr/archive/v${_version}.tar.gz"
-    URL_MD5 "88f30043996b4735baf59b020d13e25e"
+    URL_MD5 "fabbc1ad83d7c382080ca00b878e6209"
     DOWNLOAD_DIR ${CMAKE_BINARY_DIR}
     SOURCE_DIR ${CMAKE_BINARY_DIR}/${proj}
     BUILD_IN_SOURCE 1
