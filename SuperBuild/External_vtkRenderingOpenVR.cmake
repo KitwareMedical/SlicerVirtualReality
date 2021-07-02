6037cf5327ac2b4a8336496b0b5f6e9768fac65b
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2021-07-02 13:25:43 -0400

    COMP: External_vtkRenderingOpenVR: Introduce EP_*_DIR ivars
    Streamline maintenance introducing variables also used in the
    SuperBuild extension template.

--- SuperBuild/External_vtkRenderingOpenVR.cmake
+++ SuperBuild/External_vtkRenderingOpenVR.cmake
@@ -18,11 +18,7 @@ endif()
 ExternalProject_Include_Dependencies(${proj} PROJECT_VAR proj)
 
 set(VTK_SOURCE_DIR ${VTK_DIR}/../VTK)
-set(${proj}_SOURCE_DIR ${VTK_SOURCE_DIR}/Rendering/OpenVR)
 ExternalProject_Message(${proj} "VTK_SOURCE_DIR:${VTK_SOURCE_DIR}")
-ExternalProject_Message(${proj} "${proj}_SOURCE_DIR:${${proj}_SOURCE_DIR}")
-
-set(${proj}_BINARY_DIR ${CMAKE_BINARY_DIR}/${proj}-build)
 
 set(EXTERNAL_PROJECT_OPTIONAL_CMAKE_CACHE_ARGS)
 if(VTK_WRAP_PYTHON)
@@ -40,16 +36,19 @@ if(VTK_WRAP_PYTHON)
     )
 endif()
 
+set(EP_SOURCE_DIR ${VTK_SOURCE_DIR}/Rendering/OpenVR)
+set(EP_BINARY_DIR ${CMAKE_BINARY_DIR}/${proj}-build)
+
 ExternalProject_Add(${proj}
   ${${proj}_EP_ARGS}
   DOWNLOAD_COMMAND ""
   SOURCE_DIR ${VTKExternalModule_SOURCE_DIR}
-  BINARY_DIR ${${proj}_BINARY_DIR}
+  BINARY_DIR ${EP_BINARY_DIR}
   INSTALL_COMMAND ""
   CMAKE_CACHE_ARGS
     # VTKExternalModule
     -DVTK_MODULE_NAME:STRING=RenderingOpenVR
-    -DVTK_MODULE_SOURCE_DIR:PATH=${${proj}_SOURCE_DIR}
+    -DVTK_MODULE_SOURCE_DIR:PATH=${EP_SOURCE_DIR}
     -DVTK_MODULE_CMAKE_MODULE_PATH:PATH=${VTK_SOURCE_DIR}/CMake
     # vtkRenderingOpenVR
     -DCMAKE_CXX_COMPILER:FILEPATH=${CMAKE_CXX_COMPILER}
@@ -73,7 +72,7 @@ ExternalProject_Add(${proj}
     ${${proj}_DEPENDS}
   )
 
-set(${proj}_DIR ${${proj}_BINARY_DIR})
+set(${proj}_DIR ${EP_BINARY_DIR})
 
 #-----------------------------------------------------------------------------
 # Launcher setting specific to build tree
