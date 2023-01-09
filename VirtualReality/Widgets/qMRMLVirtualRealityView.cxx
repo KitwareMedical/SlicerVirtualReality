KitwareMedical/SlicerVirtualReality

commit 87fc5f983f94a2e4d1a18789a7d5fa7632aaa9c0

BUG: Fix retrieval of OpenVR last post updating VTK
See https://github.com/Slicer/Slicer/pull/6754

VirtualReality/Widgets/qMRMLVirtualRealityView.cxx
@@ -528,7 +528,7 @@ void qMRMLVirtualRealityViewPrivate::updateTransformNodeWithControllerPose(vtkEv
   }
 
   vr::TrackedDevicePose_t* tdPose;
-  this->Interactor->GetTrackedDevicePose(device, &tdPose);
+  this->RenderWindow->GetTrackedDevicePose(device, &tdPose);
 
   if (tdPose == nullptr)
   {
@@ -576,7 +576,7 @@ void qMRMLVirtualRealityViewPrivate::updateTransformNodeWithHMDPose()
   }
 
   vr::TrackedDevicePose_t* tdPose;
-  this->Interactor->GetTrackedDevicePose(vtkEventDataDevice::HeadMountedDisplay, &tdPose);
+  this->RenderWindow->GetTrackedDevicePose(vtkEventDataDevice::HeadMountedDisplay, &tdPose);
 
   if (tdPose == nullptr)
   {
@@ -625,7 +625,7 @@ void qMRMLVirtualRealityViewPrivate::updateTransformNodesWithTrackerPoses()
 
     // Now, we have our generic tracker node, let's update it!
     vr::TrackedDevicePose_t* tdPose;
-    this->Interactor->GetTrackedDevicePose(vtkEventDataDevice::GenericTracker, dev, &tdPose);
+    this->RenderWindow->GetTrackedDevicePose(vtkEventDataDevice::GenericTracker, dev, &tdPose);
 
     if (tdPose == nullptr)
     {

VirtualReality/MRML/vtkVirtualRealityViewInteractor.cxx
@@ -133,60 +133,6 @@ void vtkVirtualRealityViewInteractor::SetInteractorStyle(vtkInteractorObserver* 
 }
 
 //---------------------------------------------------------------------------
-vtkOpenVRRenderWindowInteractor::TrackerEnum vtkVirtualRealityViewInteractor::GetTrackerForDevice(vtkEventDataDevice dev)
-{
-  if (dev == vtkEventDataDevice::LeftController)
-  {
-    return vtkOpenVRRenderWindowInteractor::LEFT_HAND;
-  }
-  else if (dev == vtkEventDataDevice::RightController)
-  {
-    return vtkOpenVRRenderWindowInteractor::RIGHT_HAND;
-  }
-  else if (dev == vtkEventDataDevice::HeadMountedDisplay)
-  {
-    return vtkOpenVRRenderWindowInteractor::HEAD;
-  }
-  else
-  {
-    return vtkOpenVRRenderWindowInteractor::NUMBER_OF_TRACKERS;
-  }
-}
-
-//---------------------------------------------------------------------------
-bool vtkVirtualRealityViewInteractor::IsTrackerValid(TrackerEnum tracker)
-{
-  return tracker < vtkOpenVRRenderWindowInteractor::NUMBER_OF_TRACKERS;
-}
-
-//---------------------------------------------------------------------------
-void vtkVirtualRealityViewInteractor::GetTrackedDevicePose(vtkEventDataDevice dev, uint32_t index, vr::TrackedDevicePose_t** pose)
-{
-  TrackerEnum tracker = Self::GetTrackerForDevice(dev);
-  // TODO:
-  //
-  // Handle retrieval of tracker position associated with generic tracker associated with index.
-  //
-  // Background:
-  //
-  // Add support for generic tracker originally contributed in these commits:
-  // * kitware/VTK@6083532cf8 (ENH: Exposing the generic tracker openvr device type to downstream projects)
-  // * KitwareMedical/SlicerVirtualReality@59b7d1bf3 (Adding interface and logic to maintain MRML transforms for each generic VR tracker)
-  // but removed following the refactoring done in these commits:
-  // * kitware/VTK@1aac3fe5f (Create VRInteractorStyle and VRRenderWindowInteractor)
-  // * kitware/VTK@af0fab486 (Clean up VR and OpenVR classes)
-  // * kitware/VTK@9bd64d666 (Cleanup and rework of the VRInteractorStyle and subclasses)
-  // * kitware/VTK@09a478a22 (Cleanup VR camera code and subclasses)
-  //
-  if (!Self::IsTrackerValid(tracker))
-  {
-    *pose = nullptr;
-    return;
-  }
-  *pose = &this->Trackers[tracker].LastPose;
-}
-
-//---------------------------------------------------------------------------
 void vtkVirtualRealityViewInteractor::SetTriggerButtonFunction(std::string functionId)
 {
   vtkVirtualRealityViewInteractorStyle* vrInteractorStyle = vtkVirtualRealityViewInteractorStyle::SafeDownCast(this->InteractorStyle);

VirtualReality/MRML/vtkVirtualRealityViewInteractor.h
@@ -49,15 +49,6 @@ public:
   //
   //virtual void RecognizeComplexGesture(vtkEventDataDevice3D* edata) override;
 
-  ///@{
-  /// Get the most recent pose corresponding to the tracked device
-  void GetTrackedDevicePose(vtkEventDataDevice dev, vr::TrackedDevicePose_t** pose)
-  {
-    return this->GetTrackedDevicePose(dev, 0, pose);
-  }
-  void GetTrackedDevicePose(vtkEventDataDevice dev, uint32_t index, vr::TrackedDevicePose_t** pose);
-  ///@}
-
   /// Set trigger button function
   /// By default it is the same as grab (\sa GetButtonFunctionIdForGrabObjectsAndWorld)
   /// Empty string disables button
@@ -78,11 +69,6 @@ public:
   ///@}
 
 protected:
-  ///@{
-  /// Convenience function to map device with tracker identifier for retrieving the last tracker action.
-  static TrackerEnum GetTrackerForDevice(vtkEventDataDevice dev);
-  static bool IsTrackerValid(TrackerEnum tracker);
-  ///@}
 
   /// List of buttons for which gesture recognition is enabled
   std::vector<int> GestureEnabledButtons;

commit adfee39472fe909b573ea1c2f26e4b3be0a03dce

COMP: Fix build using renamed API to retrieve OpenVR pose
This commit fixes a regression introduced in previous commit
87fc5f9 (BUG: Fix retrieval of OpenVR last post updating VTK) using
the expected function name.

VirtualReality/Widgets/qMRMLVirtualRealityView.cxx
@@ -528,7 +528,7 @@ void qMRMLVirtualRealityViewPrivate::updateTransformNodeWithControllerPose(vtkEv
   }
 
   vr::TrackedDevicePose_t* tdPose;
-  this->RenderWindow->GetTrackedDevicePose(device, &tdPose);
+  this->RenderWindow->GetOpenVRPose(device, &tdPose);
 
   if (tdPose == nullptr)
   {
@@ -576,7 +576,7 @@ void qMRMLVirtualRealityViewPrivate::updateTransformNodeWithHMDPose()
   }
 
   vr::TrackedDevicePose_t* tdPose;
-  this->RenderWindow->GetTrackedDevicePose(vtkEventDataDevice::HeadMountedDisplay, &tdPose);
+  this->RenderWindow->GetOpenVRPose(vtkEventDataDevice::HeadMountedDisplay, &tdPose);
 
   if (tdPose == nullptr)
   {
@@ -625,7 +625,7 @@ void qMRMLVirtualRealityViewPrivate::updateTransformNodesWithTrackerPoses()
 
     // Now, we have our generic tracker node, let's update it!
     vr::TrackedDevicePose_t* tdPose;
-    this->RenderWindow->GetTrackedDevicePose(vtkEventDataDevice::GenericTracker, dev, &tdPose);
+    this->RenderWindow->GetOpenVRPose(vtkEventDataDevice::GenericTracker, dev, &tdPose);
 
     if (tdPose == nullptr)
     {