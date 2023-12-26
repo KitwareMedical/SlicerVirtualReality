8d8f2b00b06d497481eec55d7c18020147bce380
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2023-12-26 11:55:25 -0500

    DOC: Improve VirtualRealityViewNode doxygen grouping members
    See https://www.doxygen.nl/manual/grouping.html#memgroup

--- VirtualReality/MRML/vtkMRMLVirtualRealityViewNode.h
+++ VirtualReality/MRML/vtkMRMLVirtualRealityViewNode.h
@@ -58,9 +58,11 @@ public:
   /// Update the references of the node to the scene.
   void SetSceneReferences() override;
 
+  ///@{
   /// Return the color the view nodes have for the background by default.
   static double* defaultBackgroundColor();
   static double* defaultBackgroundColor2();
+  ///}@
 
   /// Get reference view node.
   /// Reference view node is a regular 3D view node, which content
@@ -131,61 +133,82 @@ public:
   /// \sa SetAndObserveTrackerTransformNode
   void RemoveAllTrackerTransformNodes();
 
+  ///@{
   /// Controls two-sided lighting property of the renderer
   vtkGetMacro(TwoSidedLighting, bool);
   vtkSetMacro(TwoSidedLighting, bool);
   vtkBooleanMacro(TwoSidedLighting, bool);
+  ///}@
 
+  ///@{
   /// If enabled then 4 lights are used, otherwise just 2.
   vtkGetMacro(BackLights, bool);
   vtkSetMacro(BackLights, bool);
   vtkBooleanMacro(BackLights, bool);
+  ///}@
 
+  ///@{
   /// Desired frame rate. Volume renderer may use this information
   /// for determining sampling distances (and other LOD actors, to
   /// determine display quality).
   vtkGetMacro(DesiredUpdateRate, double);
   vtkSetMacro(DesiredUpdateRate, double);
+  ///}@
 
+  ///@{
   /// Magnification of world [0.01, 100].
   /// Value greater than 1 means that objects appear larger in VR than their real world size.
   /// Translated to physical scale of the VR render window
   vtkGetMacro(Magnification, double);
   vtkSetMacro(Magnification, double);
+  ///}@
 
+  ///@{
   /// Motion speed of fly (i.e. dolly) in meters per second.
   /// Default is walking speed: 6 km/h = 1.66 m/s
   vtkGetMacro(MotionSpeed, double);
   vtkSetMacro(MotionSpeed, double);
+  ///}@
 
   /// Motion sensitivity (between 0.0 and 1.0).
   /// If virtual reality headset is not moving then update rate
   /// is decreased to allow higher quality rendering.
   vtkGetMacro(MotionSensitivity, double);
   vtkSetMacro(MotionSensitivity, double);
+  ///}@
 
+  ///@{
   /// If enabled then pose of controllers are saved in the scene as transforms.
   vtkGetMacro(ControllerTransformsUpdate, bool);
   void SetControllerTransformsUpdate(bool enable);
   vtkBooleanMacro(ControllerTransformsUpdate, bool);
+  ///}@
 
+  ///@{
   vtkGetMacro(HMDTransformUpdate, bool);
   void SetHMDTransformUpdate(bool enable);
   vtkBooleanMacro(HMDTransformUpdate, bool);
+  ///}@
 
+  ///@{
   vtkGetMacro(TrackerTransformUpdate, bool);
   void SetTrackerTransformUpdate(bool enable);
   vtkBooleanMacro(TrackerTransformUpdate, bool);
+  ///}@
 
+  ///@{
   /// If set to true then controllers are visible in virtual reality view.
   vtkGetMacro(ControllerModelsVisible, bool);
   vtkSetMacro(ControllerModelsVisible, bool);
   vtkBooleanMacro(ControllerModelsVisible, bool);
+  ///}@
 
+  ///@{
   /// If set to true then tracking references (Lighthouses) are visible in virtual reality view.
   vtkGetMacro(LighthouseModelsVisible, bool);
   vtkSetMacro(LighthouseModelsVisible, bool);
   vtkBooleanMacro(LighthouseModelsVisible, bool);
+  ///}@
 
   /// Return true if an error has occurred.
   /// "Connected" member requests connection but this method can tell if the
